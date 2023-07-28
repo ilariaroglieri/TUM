b644846974602755009992e583209d3ed4657eeb
Ilaria Roglieri <ilariaroglieri@gmail.com>
2023-07-28 13:14:39 +0200

    created page template for past editions

---
+++ page-edizioni-precedenti.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+
+<?php $currentID = $wp_query->queried_object_id; ?>
+
+<section class="content" id="content-previous-years">
+  <?php
+    $terms = get_terms( array(
+      'taxonomy' => 'event_year',
+      'orderby' => 'slug',
+      'order' => 'DESC',
+    ));
+  ?>
+
+  <?php foreach( $terms as $term ) : ?>
+    <div class="container-fluid event-year-row">
+      <h2 class="event-year-link spacing-p-t-1 s-large uppercase"><a href="<?php echo get_term_link($term->term_id); ?>">TUM <?= $term->name; ?></a></h2>
+    </div>
+  <?php endforeach; ?>
+
+</section>
+
+<?php get_footer(); ?>
